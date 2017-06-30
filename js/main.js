c53bd8ac41b2f142471f6677b3fec76f783b915e
carlloganjr <carlloganjr@gmail.com>
2017-06-30 10:08:54 -0700

    click event added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const vid = document.querySelector('video');
   const spanList = document.querySelectorAll('.spanList');
+  const div = document.querySelector('.videoWrapper');
   const playTimes = [0.240, 4.130, 7.535, 11.270, 13.960, 17.940, 22.370,
                     26.880, 32.100, 34.730, 39.430, 42.350, 46.300, 49.270,
                     53.760, 57.780, 60.150];
 
 
   vid.addEventListener('play', () => {
-    vid.addEventListener('timeupdate', (e) => {
+    vid.addEventListener('timeupdate', () => {
       for (let i = 0; i < spanList.length; i += 1) {
         let highlight = spanList[i];
         let start = playTimes[i];
@@ -24,4 +25,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     });
+
+    div.addEventListener('click', (e) => {
+      for (let i = 0; i < spanList.length; i += 1) {
+        let click = spanList[i];
+        let position = playTimes[i];
+
+        if (e.target === click) {
+          vid.currentTime = position;
+        }
+      }
+    });
+
   });
