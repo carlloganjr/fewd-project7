9c1e33ccd46bd5a1b49063384563e09cc1612de8
carlloganjr <carlloganjr@gmail.com>
2017-06-30 08:50:57 -0700

    loop updated and complete

--- js/main.js
+++ js/main.js
@@ -1,38 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const vid = document.querySelector('video');
   const spanList = document.querySelectorAll('.spanList');
-  const playTimes = [0.200, 0.240, 4.130, 7.535, 11.270, 13.960, 17.940, 22.370,
+  const playTimes = [0.240, 4.130, 7.535, 11.270, 13.960, 17.940, 22.370,
                     26.880, 32.100, 34.730, 39.430, 42.350, 46.300, 49.270,
                     53.760, 57.780, 60.150];
 
 
   vid.addEventListener('play', () => {
     vid.addEventListener('timeupdate', (e) => {
-      for (let i = 1; i < spanList.length; i += 1) {
+      for (let i = 0; i < spanList.length; i += 1) {
         let highlight = spanList[i];
         let start = playTimes[i];
         let end = playTimes[i + 1];
-        let finished = spanList[i - 1];
         let videoTime = vid.currentTime;
 
         if (videoTime > start && videoTime < end) {
-          finished.style.color = 'red';
+          highlight.style.color = '#ffb900';
           } else if (videoTime > start && videoTime > end) {
-              highlight.style.color = 'black';
-          } else if (videoTime > start && videoTime > end) {
-              highlight.style.color = 'black';
+              highlight.style.color = '#000';
           } else {
-              highlight.style.color = 'black';
+              highlight.style.color = '#000';
           }
         }
       });
     });
   });
-
-// if (videoTime > start && start < end) {
-//   highlight.style.color = 'red';
-// } else if () {
-//
-// } else {
-//     highlight.style.color = 'black';
-// }
